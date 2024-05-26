b29749988b94581f2d50b645778b5ce5c325ec3f
ian santos <iancosta99gk@gmail.com>
2024-05-26 12:27:05 -0300

    clear comments

--- PB/tp4/grupo5/telnet_cliente.py
+++ PB/tp4/grupo5/telnet_cliente.py
@@ -3,24 +3,21 @@ import telnetlib3
 
 async def telnet_client(host, port):
     try:
-        # Conectar ao servidor Telnet
         reader, writer = await telnetlib3.open_connection(host, port)
         print(f"Conectado ao servidor Telnet {host} na porta {port}")
 
-        # Interação com o servidor
         writer.write("Olá, servidor!\n")
         await writer.drain()
 
         response = await reader.read(1024)
         print(f"Resposta do servidor: {response}")
 
-        # Fechar conexão
         writer.close()
         await writer.wait_closed()
     except Exception as e:
         print(f"Erro ao conectar ao servidor Telnet: {e}")
 
 if __name__ == "__main__":
-    host = "127.0.0.1"  # Endereço do servidor
-    port = 23           # Porta do servidor
+    host = "127.0.0.1"  
+    port = 23  
     asyncio.run(telnet_client(host, port))
