a438e9c6b37e532aa840f2919ad7c9f0fa077c0c
Ian Santos <ian.santos@leste.com>
2024-06-10 18:21:27 -0300

    grupo 4

--- PB/tp5/grupo4.py
+++ PB/tp5/grupo4.py
@@ -1,31 +1,43 @@
 from scapy.all import *
 
-# Exercício 10: Implementar um sniffer de pacotes
-# https://scapy.readthedocs.io/en/latest/usage.html#asynchronous-sniffing
-a = sniff(count=30)
-a.nsummary()
+def sniff_packets():
+  a = sniff(count=30)
+  a.nsummary()
 
-print('\n----------------------------------------------------------------------------\n')
+def port_scanning_and_traceroute():
+  res, unans = sr(IP(dst="187.16.106.206") / TCP(flags="S", dport=(1, 100)))
+  res.nsummary(lfilter=lambda s, r: (r.haslayer(TCP) and (r.getlayer(TCP).flags & 2)))
 
-# Exercício 11: Implementar um programa para executar port scanning e Traceroute
-# https://scapy.readthedocs.io/en/latest/usage.html#tcp-port-scanning
-res, unans = sr( IP(dst="187.16.106.206" )
-                /TCP(flags="S", dport=(1,100)))
-res.nsummary( lfilter=lambda s,r: (r.haslayer(TCP) and (r.getlayer(TCP).flags & 2)) )
-
-print('\n')
-
-# https://scapy.readthedocs.io/en/latest/usage.html#tcp-traceroute
-ans, unans = sr(IP(dst="187.16.106.206", ttl=(4,25),id=RandShort())/TCP(flags=0x2))
-for snd, rcv in ans:
+  ans, unans = sr(IP(dst="187.16.106.206", ttl=(4, 25), id=RandShort()) / TCP(flags=0x2))
+  for snd, rcv in ans:
     print(snd.ttl, rcv.src, isinstance(rcv.payload, TCP))
 
-print('\n----------------------------------------------------------------------------\n')
+def detect_arp_spoofing():
+  def arp_spoof_detection(packet):
+    print("ARP Reply detected from: " + packet[ARP].hwsrc + " for IP: " + packet[ARP].psrc)
 
-# Exercício 12: Implementar um programa que detecta ARP spoofing
-# https://scapy.readthedocs.io/en/latest/api/scapy.layers.inet.html#scapy.layers.inet.connect_from_ip
-def arp_spoof_detection(packet):
-  print("ARP Reply detected from: " + packet[ARP].hwsrc + " for IP: " + packet[ARP].psrc)
+  sniff(prn=arp_spoof_detection, filter="arp", store=0, count=1)
 
-sniff(prn=arp_spoof_detection, filter="arp", store=0, count=1)
+def main():
+  print("Escolha uma opção:")
+  print("1. Sniff de pacotes")
+  print("2. Port Scanning e Traceroute")
+  print("3. Detectar ARP Spoofing")
+  print("4. Sair")
 
+  while True:
+    choice = input("Digite o número da opção desejada: ")
+    if choice == '1':
+      sniff_packets()
+    elif choice == '2':
+      port_scanning_and_traceroute()
+    elif choice == '3':
+      detect_arp_spoofing()
+    elif choice == '4':
+      print("Saindo...")
+      break
+    else:
+      print("Opção inválida. Por favor, escolha uma opção válida.")
+
+if __name__ == "__main__":
+  main()
