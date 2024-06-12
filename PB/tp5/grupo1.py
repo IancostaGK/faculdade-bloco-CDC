5a8c944872c330b1d55c158ce76cb61ed0c6df07
Ian Santos <ian.santos@leste.com>
2024-06-12 13:04:05 -0300

    remove comments

--- PB/tp5/grupo1.py
+++ PB/tp5/grupo1.py
@@ -2,11 +2,6 @@ import grafo
 import heap
 
 def caminho_mais_curto_dijkstra(v_orig, v_dest):
-  """
-  Implementa o algoritmo de Dijkstra
-  Retorna o caminho mais curto entre v_orig e v_dest
-  """
-  
   menor_peso = {}
   predecessor = {}
   visitados = set()
@@ -15,27 +10,18 @@ def caminho_mais_curto_dijkstra(v_orig, v_dest):
   v_atual = v_orig
   
   while v_atual:
-    # Visita o vértice atual
     visitados.add(v_atual)
-    # Verifica as arestas para seus vizinhos
     for vizinho, peso in v_atual.vizinhos.items():
-      # Calcula peso total do caminho de 
-      # v_orig a vizinho via v_atual
       peso_total = menor_peso[v_atual] + peso
-      # Se for o caminho mais curto (até o momento)
-      # de v_orig a vizinho, atualiza as tabelas
       if (vizinho not in menor_peso or 
           peso_total < menor_peso[vizinho]):
         menor_peso[vizinho] = peso_total
         predecessor[vizinho] = v_atual
-    # Seleciona o vértice não visitado com
-    # menor valor na tabela de pesos
     v_atual = None
     for v in menor_peso:
       if (v not in visitados and 
           (not v_atual or menor_peso[v] < menor_peso[v_atual])):
         v_atual = v
-  # Reconstrói e retorna o caminho mais curto
   caminho = []
   v_atual = v_dest
   while v_atual != v_orig:
@@ -51,7 +37,7 @@ def kruskal_mst(self):
     for vertice in self.vertices:
       for vizinho, peso in vertice.vizinhos.items():
         edges.append((peso, vertice, vizinho))
-    edges = sorted(edges, key=lambda x: x[0])  # Ordena arestas por peso
+    edges = sorted(edges, key=lambda x: x[0]) 
 
     parent = {}
     rank = {}
@@ -86,12 +72,6 @@ def kruskal_mst(self):
 
 
 def agm_prim(v_orig):
-  """
-  Implementa o algoritmo de Prim
-  Retorna um conjunto de arestas formando uma árvore
-  geradora mínima encontrada a partir de v_orig
-  """
-  
   def peso_aresta(aresta):
     return aresta[2]
 
@@ -99,30 +79,21 @@ def agm_prim(v_orig):
   vertices_agm = set()
   arestas = heap.Heap(chave=peso_aresta, max_heap=False)
 
-  # Adiciona o vértice de origem na AGM
   v_atual = v_orig
   vertices_agm.add(v_atual)
 
   while True:
-    # Verifica as arestas para vizinhos do vértice atual
     for vizinho, peso in v_atual.vizinhos.items():
-      # Se o vizinho não está na AGM, insere a aresta no heap
       if vizinho not in vertices_agm:
         arestas.insere((v_atual, vizinho, peso))
-    # Seleciona a aresta de menor peso no heap para um
-    # vértice que não esteja na AGM
     aresta = arestas.remove()
     while not arestas.vazio() and aresta[1] in vertices_agm:
       aresta = arestas.remove()
-    # Se não encontrar uma aresta, encerra
     if aresta is None or aresta[1] in vertices_agm:
       break
-    # Caso contrário, insere novo vértice na AGM,
-    # conectado pela aresta selecionada
     v_atual = aresta[1]
     vertices_agm.add(v_atual)
     arestas_agm.add(aresta)
-  # Retorna arestas da árvore geradora mínima
   return arestas_agm
 
 cidades = grafo.GrafoPonderado()
